552ac735a06504b33867f87297c02a013165e113
Jose Neyra <jose-neyra@outlook.com>
2022-08-17 10:23:03 -0400

    Initial project setup

---
+++ sf-ms-brewery/src/main/java/com/joseneyra/sfmsbrewery/SfMsBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.joseneyra.sfmsbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfMsBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfMsBreweryApplication.class, args);
+	}
+
+}
---
+++ sf-ms-brewery/src/test/java/com/joseneyra/sfmsbrewery/SfMsBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseneyra.sfmsbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfMsBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
