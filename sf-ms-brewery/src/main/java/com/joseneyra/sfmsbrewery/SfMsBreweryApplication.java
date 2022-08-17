f703bbcb7d7900068abb2e696dd3deea5f403832
Jose Neyra <jose-neyra@outlook.com>
2022-08-17 10:33:13 -0400

    Test commit

--- sf-ms-brewery/src/main/java/com/joseneyra/sfmsbrewery/SfMsBreweryApplication.java
+++ sf-ms-brewery/src/main/java/com/joseneyra/sfmsbrewery/SfMsBreweryApplication.java
@@ -1,12 +1,16 @@
 package com.joseneyra.sfmsbrewery;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SfMsBreweryApplication {
 
 	public static void main(String[] args) {
+
+		log.info("Application started");
 		SpringApplication.run(SfMsBreweryApplication.class, args);
 	}
 
